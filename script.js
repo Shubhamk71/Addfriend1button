1573c29c4ed30b4d2268a205c27b2947419a7bb6
Shubham kumar <134083767+Shubhamk71@users.noreply.github.com>
2024-02-22 17:12:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var istatus = document.querySelector("h5")
+var btn = document.querySelector("#add")
+var check=0
+
+btn.addEventListener("click" ,function(){
+    if(check==0){
+        istatus.innerHTML = "friends"
+        istatus.style.color="green"
+        btn.innerHTML="remove friend"
+      check=1
+    }
+    else{
+        istatus.innerHTML="stranger"
+        istatus.style.color="red"
+        btn.innerHTML="Add friend"
+        check=0
+
+    }
+
+})
